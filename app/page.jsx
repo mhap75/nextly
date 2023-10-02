c5a7cdf7c6eb1f30c7bf569d20ca3cc7ced4ca1d
Mohammad Hossein Amirpanahi <118535877+mhap75@users.noreply.github.com>
2023-10-02 17:28:47 +0330

    Add TODOs

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,16 @@
-import Script from "next/script";
-import {AddSchedule} from "@/components";
+import { AddSchedule } from "@/components";
+import { getEvents } from "@/lib/getEvents";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getEvents();
+
+  // ToDo: Using api routes, events will be fetched from the GOOGLE CALENDER api
+  // ToDo: In the events list, there will be a search function and the EventCard component for each event. It will be a CLIENT component.
+
   return (
     <section className="h-[600px]">
       <AddSchedule />
+      {/* <EventsList events={events} /> */}
     </section>
   );
 }
